2e29ce623f437cf18177be61493e7e1ca54e6e8a
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-09 00:27:08 +0100

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,29 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
-
+  app: {
+    head: {
+      title: "Lionel Kaniki | Portfolio",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Portfolio de lionel Kaniki.",
+        },
+        { name: "format-detection", content: "telephone-no" },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+      ],
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
