d61997c1df58b6f92a3010f3555dec880af7a73c
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-08 05:57:57 +0100

    add header

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="max-w-4xl w-full mx-auto py-10 px-6">
+    <!-- Header -->
+    <header>
+      <div class="flex flex-col-reverse xl:flex-row items-center pt-10 xl:pt-0 xl:gap-28">
+        <div>
+          <h3 class="text-3xl font-bold">🖐 I'm Lionel Kaniki</h3>
+          <p class="my-2 text-xl">Fullstack Developer. I'm passionate about challenges and detail-oriented. With my 5 years of experience, I design tailor-made web platforms that adapt to clients needs.</p>
+          <div class="flex gap-1 items-center">
+            <Icon name="ph:map-pin-area" class="shrink-0 w-6 h-6" />
+            <p class="text-lg -mt-1">Kinshasa, Republique Democratique du Congo</p>
+          </div>
+        </div>
+        <div class="size-16 shrink-0 md:size-28">
+          <img class="rounded-full drop-shadow-lg" src="" alt="photo">
+        </div>
+      </div>
+      <!-- Credentials -->
+      <div class="flex items-center my-2 gap-2">
+        <button type="button" class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 hover:bg-gray-100 disabled:bg-gray-50 aria-disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 dark:aria-disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-gray-500 dark:focus-visible:ring-gray-400 inline-flex items-center">
+          <Icon name="ph:envelope" class="shrink-0 w-8 h-8 text-gray-700" />
+        </button>
+        <NuxtLink class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 hover:bg-gray-100 disabled:bg-gray-50 aria-disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 dark:aria-disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-gray-500 dark:focus-visible:ring-gray-400 inline-flex items-center">
+          <Icon name="ph:phone-call" class="shrink-0 w-8 h-8 text-gray-700" />
+        </NuxtLink>
+        <NuxtLink class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 hover:bg-gray-100 disabled:bg-gray-50 aria-disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 dark:aria-disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-gray-500 dark:focus-visible:ring-gray-400 inline-flex items-center">
+          <Icon name="ph:github-logo" class="shrink-0 w-8 h-8 text-gray-700" />
+        </NuxtLink>
+        <NuxtLink class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 hover:bg-gray-100 disabled:bg-gray-50 aria-disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 dark:aria-disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-gray-500 dark:focus-visible:ring-gray-400 inline-flex items-center">
+          <Icon name="ph:linkedin-logo" class="shrink-0 w-8 h-8 text-gray-700" />
+        </NuxtLink>
+        <NuxtLink class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 hover:bg-gray-100 disabled:bg-gray-50 aria-disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 dark:aria-disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-gray-500 dark:focus-visible:ring-gray-400 inline-flex items-center">
+          <Icon name="ph:x-logo" class="shrink-0 w-8 h-8 text-gray-700" />
+        </NuxtLink>
+      </div>
+    </header>
+  </div>
+
+</template>
+<script setup>
+
+</script>
