1adb73ba3502f3611d644e2152a5e4d13ea4a525
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-11 21:18:01 +0100

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: "Lionel Kaniki | Portfolio",
+      title: "Lionel Kaniki",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ export default {
   theme: {
     fontFamily: {
       'sans': [
-        'Fira Code'
+        'Montserrat'
       ],
     },
     container: {
