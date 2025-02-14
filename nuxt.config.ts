5e1549542359c6f646e547cffe8e15f61309d0b2
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-14 13:15:37 +0100

    add nuxtjs/i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  modules: ['@nuxt/icon', "@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxtjs/i18n"],
+  i18n: {
+    langDir: "locales",
+    locales: [
+      {code : 'en', language: 'en-US', name: 'English', file: 'en.json'},
+      {code : 'fr', language: 'fr-FR', name: 'Français', file: 'fr.json'},
+    ],
+    lazy: true,
+    defaultLocale: 'en',
+    strategy: "prefix_except_default",
+    vueI18n: './i18n.config.ts',
+  },
   tailwindcss: {
     cssPath: 'assets/css/tailwind.css'
   },
@@ -39,7 +51,6 @@ export default defineNuxtConfig({
       collections: ['ph'],
     },
   },
-  modules: ['@nuxt/icon', "@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
   colorMode : {
     classSuffix: '',
     preference: 'system',
