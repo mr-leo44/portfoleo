56c8579873d84326f4f8104968bf585613833ae6
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-14 13:16:43 +0100

    add a lang switcher component

---
+++ components/LanguageSwitcher.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+
+const { locale, locales , setLocale } = useI18n()
+</script>
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline">
+        {{ locale.toUpperCase() }}
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem v-for="lang in locales" :key="lang.code" @click="setLocale(lang.code)">
+        {{ lang.name }}
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
