9abb8a428594628de4e67ad63ae65283fd85b3d2
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-15 20:52:02 +0100

    add locale images

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -8,12 +8,36 @@ const { locale, locales , setLocale } = useI18n()
   <DropdownMenu>
     <DropdownMenuTrigger as-child>
       <Button variant="outline">
-        {{ locale.toUpperCase() }}
+        <div v-if="locale === 'fr'">
+          <div>
+            <img class="size-4 shrink-0" src="~/assets/img/locales/france.svg" alt="FR">
+          </div>
+        </div>
+        <div v-else>
+          <div>
+            <img class="size-4 shrink-0" src="~/assets/img/locales/usa.svg" alt="EN">
+          </div>
+        </div>
       </Button>
     </DropdownMenuTrigger>
     <DropdownMenuContent align="end">
       <DropdownMenuItem v-for="lang in locales" :key="lang.code" @click="setLocale(lang.code)">
-        {{ lang.name }}
+        <div v-if="lang.name === 'Français'" class="flex gap-x-2 items-center">
+          <div>
+            <img class="size-4 shrink-0" src="~/assets/img/locales/france.svg" alt="FR">
+          </div>
+          <div>
+            {{ lang.name }}
+          </div>
+        </div>
+        <div v-else class="flex gap-x-2 items-center">
+          <div>
+            <img class="size-4 shrink-0" src="~/assets/img/locales/usa.svg" alt="EN">
+          </div>
+          <div>
+            {{ lang.name }}
+          </div>
+        </div>
       </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
